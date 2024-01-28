f4748ab9739611bd70a91ec7970af26f22522282
hasanonik <jh8539615@gmail.com>
2024-01-28 14:29:27 +0600

    first commit

---
+++ assets/js/language.js
@@ -0,0 +1,227 @@
+
+
+
+/*--english--*/
+$(".english").click(function(){
+    $(".english-text").show();
+})
+
+$(".english").click(function(){
+    $(".spanish-text").hide();
+})
+
+$(".english").click(function(){
+    $(".china-text").hide();
+})
+
+$(".english").click(function(){
+    $(".german-text").hide();
+})
+
+$(".english").click(function(){
+    $(".russia-text").hide();
+})
+
+$(".english").click(function(){
+    $(".arabic-text").hide();
+})
+
+$(".english").click(function(){
+    $(".france-text").hide();
+})
+/*----*/
+
+
+
+
+/*---spanish--*/
+$(".spanish").click(function(){
+    $(".spanish-text").show();
+})
+
+$(".spanish").click(function(){
+    $(".english-text").hide();
+})
+
+$(".spanish").click(function(){
+    $(".china-text").hide();
+})
+
+$(".spanish").click(function(){
+    $(".german-text").hide();
+})
+
+$(".spanish").click(function(){
+    $(".russia-text").hide();
+})
+
+$(".spanish").click(function(){
+    $(".arabic-text").hide();
+})
+
+$(".spanish").click(function(){
+    $(".france-text").hide();
+})
+/*----*/
+
+
+
+/*---china--*/
+$(".china").click(function(){
+    $(".china-text").show();
+})
+
+$(".china").click(function(){
+    $(".english-text").hide();
+})
+
+$(".china").click(function(){
+    $(".spanish-text").hide();
+})
+
+$(".china").click(function(){
+    $(".german-text").hide();
+})
+
+$(".china").click(function(){
+    $(".russia-text").hide();
+})
+
+$(".china").click(function(){
+    $(".arabic-text").hide();
+})
+
+$(".china").click(function(){
+    $(".france-text").hide();
+})
+/*----*/
+
+
+
+/*---german--*/
+$(".german").click(function(){
+    $(".german-text").show();
+})
+
+$(".german").click(function(){
+    $(".english-text").hide();
+})
+
+$(".german").click(function(){
+    $(".spanish-text").hide();
+})
+
+$(".german").click(function(){
+    $(".china-text").hide();
+})
+
+$(".german").click(function(){
+    $(".russia-text").hide();
+})
+
+$(".german").click(function(){
+    $(".arabic-text").hide();
+})
+
+$(".german").click(function(){
+    $(".france-text").hide();
+})
+/*----*/
+
+
+
+/*---russia--*/
+$(".russia").click(function(){
+    $(".russia-text").show();
+})
+
+$(".russia").click(function(){
+    $(".english-text").hide();
+})
+
+$(".russia").click(function(){
+    $(".spanish-text").hide();
+})
+
+$(".russia").click(function(){
+    $(".china-text").hide();
+})
+
+$(".russia").click(function(){
+    $(".german-text").hide();
+})
+
+$(".russia").click(function(){
+    $(".arabic-text").hide();
+})
+
+$(".russia").click(function(){
+    $(".france-text").hide();
+})
+/*----*/
+
+
+
+
+/*---arabic--*/
+$(".arabic").click(function(){
+    $(".arabic-text").show();
+})
+
+$(".arabic").click(function(){
+    $(".english-text").hide();
+})
+
+$(".arabic").click(function(){
+    $(".spanish-text").hide();
+})
+
+$(".arabic").click(function(){
+    $(".china-text").hide();
+})
+
+$(".arabic").click(function(){
+    $(".german-text").hide();
+})
+
+$(".arabic").click(function(){
+    $(".russia-text").hide();
+})
+
+$(".arabic").click(function(){
+    $(".france-text").hide();
+})
+/*----*/
+
+
+
+
+/*---france--*/
+$(".france").click(function(){
+    $(".france-text").show();
+})
+
+$(".france").click(function(){
+    $(".english-text").hide();
+})
+
+$(".france").click(function(){
+    $(".spanish-text").hide();
+})
+
+$(".france").click(function(){
+    $(".china-text").hide();
+})
+
+$(".france").click(function(){
+    $(".german-text").hide();
+})
+
+$(".france").click(function(){
+    $(".russia-text").hide();
+})
+
+$(".france").click(function(){
+    $(".arabic-text").hide();
+})
+/*----*/
---
+++ assets/js/main.js
@@ -0,0 +1,264 @@
+
+/*------*/
+var input = document.querySelector(".input-box");
+input.onclick = function () {
+  this.classList.toggle("open");
+  let list = this.nextElementSibling;
+  if (list.style.maxHeight) {
+    list.style.maxHeight = null;
+    list.style.boxShadow = null;
+  } else {
+    list.style.maxHeight = list.scrollHeight + "px";
+    list.style.boxShadow =
+      "0 1px 2px 0 rgba(0, 0, 0, 0.15),0 1px 3px 1px rgba(0, 0, 0, 0.1)";
+  }
+};
+
+var rad = document.querySelectorAll(".radio");
+rad.forEach((item) => {
+  item.addEventListener("change", () => {
+    input.innerHTML = item.nextElementSibling.innerHTML;
+    input.click();
+  });
+});
+
+// var label = document.querySelectorAll("label");
+// function search(searchin) {
+//   let searchVal = searchin.value;
+//   searchVal = searchVal.toUpperCase();
+//   label.forEach((item) => {
+//     let checkVal = item.querySelector(".name").innerHTML;
+//     checkVal = checkVal.toUpperCase();
+//     if (checkVal.indexOf(searchVal) == -1) {
+//       item.style.display = "none";
+//     } else {
+//       item.style.display = "flex";
+//     }
+//     let list = input.nextElementSibling;
+//     list.style.maxHeight = list.scrollHeight + "px";
+//   });
+// }
+/*-----*/
+
+
+
+/*-----*/
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 20
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
+
+})()
+/*-----*/
+
+
+
+
+
+
